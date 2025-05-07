f35cb838c5d5241b8aaf7bfdcfc5fd704b4fbb01
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-07 14:54:44 -0300

    Add views (feed, post_create, post_delete, comment_create, comment_delete)

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 # Create your views here.
 
@@ -34,3 +35,54 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+# View for "Logged-in User Feed" and "Logged-out User Feed"
+def feed(request):
+    posts = Post.objects.all().order_by('-created_at')
+    if request.user.is_authenticated:
+        return render(request, 'feed.html', {'posts': posts})
+    else:
+        return render(request, 'guest_feed.html', {'posts': posts})
+
+# View for Creating a Post
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('feed')
+    else:
+        form = PostForm()
+    return render(request, 'post_create.html', {'form': form})
+
+# View for Deleting a Post
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk, user=request.user)
+    post.delete()
+    return redirect('feed')
+
+# View for Commenting on a Post
+@login_required
+def comment_create(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+            return redirect('feed')
+    else:
+        form = CommentForm()
+    return render(request, 'comment_create.html', {'form': form, 'post': post})
+
+# View for Deleting a Comment
+@login_required
+def comment_delete(request, pk):
+    comment = get_object_or_404(Comment, pk=pk, user=request.user)
+    comment.delete()
+    return redirect('feed')
