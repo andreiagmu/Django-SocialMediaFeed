285cebea478fedd3e18d827911a5242d17dabbb7
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-13 11:00:13 -0300

    Write forms.py

---
+++ core/forms.py
@@ -0,0 +1,20 @@
+﻿from django import forms
+from django.contrib.auth.models import User
+
+from core.models import Post, Comment
+
+
+class UsernameChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['content']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
