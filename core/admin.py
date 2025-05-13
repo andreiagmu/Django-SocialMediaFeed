42789f02998c0a923a490d2686210c79f25293ec
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2025-05-13 15:39:23 -0300

    admin.py: Register models (Post, Comment, Like)

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from core.models import Post, Comment, Like
+
+
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
